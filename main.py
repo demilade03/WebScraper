875dae43789e1809e64b51c270b0ed24053c7544
demilade03 <odemilade118@gmail.com>
2024-05-04 15:56:25 +0100

    Add webpage to file

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 
 user_url = input("Enter a URL: ")
 response = requests.get(user_url, headers={"Accept-Language": "en-US,en;q=0.5"})
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "lxml")
-    title_tag = soup.title
-    meta_tag = soup.meta
-    print(title_tag.text, meta_tag)
+    page_content = response.content
+    with open("source.html", "wb") as file:
+        file.write(page_content)
+    print("Contents saved")
 else:
-    print("Invalid quote resource!")
+    print(f"The URL returned {response.status_code}!")
