fe1eb38ffce033b396d0099695609d05b3d71116
demilade03 <odemilade118@gmail.com>
2024-05-06 16:01:26 +0100

    Changed file name

--- scraper.py
+++ scraper.py
@@ -2,22 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    user_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
-    response = requests.get(user_url, headers={"Accept-Language": "en-US,en;q=0.5"})
+user_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
+response = requests.get(user_url, headers={"Accept-Language": "en-US,en;q=0.5"})
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "lxml")
-        article_tag = soup.find_all("article")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "lxml")
+    article_tag = soup.find_all("article")
 
-        for tag in article_tag:
-            span_tag = tag.span
-            try:
-                if span_tag["data-test"]:
-                    print(f"\n{span_tag.string}")
-                    print(
-                        "===============================================================================================\n")
-            except KeyError:
-                continue
-    else:
-        print(f"The URL returned {response.status_code}!")
+    for tag in article_tag:
+        span_tag = tag.span
+        try:
+            if span_tag["data-test"]:
+                print(f"\n{span_tag.string}")
+                print(
+                    "===============================================================================================\n")
+        except KeyError:
+            continue
+else:
+    print(f"The URL returned {response.status_code}!")
