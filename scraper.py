73017e0828230ac2589c2426c3e9ce93b0fb5321
demilade03 <odemilade118@gmail.com>
2024-05-07 15:32:25 +0100

    Got paragraphs from links in <a> tag

--- scraper.py
+++ scraper.py
@@ -8,15 +8,19 @@ response = requests.get(user_url, headers={"Accept-Language": "en-US,en;q=0.5"})
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "lxml")
     article_tag = soup.find_all("article")
+    article_links = []
 
     for tag in article_tag:
         span_tag = tag.span
-        try:
-            if span_tag["data-test"]:
-                print(f"\n{span_tag.string}")
-                print(
-                    "===============================================================================================\n")
-        except KeyError:
+        if "data-test" not in span_tag.attrs.keys():
             continue
+        article = requests.get(f"{user_url + tag.a['href']}")
+        article_soup = BeautifulSoup(article.content, "lxml")
+        article_content = article_soup.find_all("p")
+
+        for article_tag in article_content:
+            print(f"\n{article_tag}")
+            print("-------------------------------------------------------------------------------------------------")
+            print("-------------------------------------------------------------------------------------------------\n")
 else:
     print(f"The URL returned {response.status_code}!")
