56f0211b57fba9a037ce468eaabddff5a38d9f9e
demilade03 <odemilade118@gmail.com>
2024-05-08 20:37:19 +0100

    Broke the code into two functions 'main' and 'get_web_response'

--- scraper.py
+++ scraper.py
@@ -2,25 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 
-user_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
-response = requests.get(user_url, headers={"Accept-Language": "en-US,en;q=0.5"})
-
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "lxml")
+def main():
+    user_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
+    soup = get_web_response(user_url)
     article_tag = soup.find_all("article")
-    article_links = []
 
     for tag in article_tag:
         span_tag = tag.span
         if "data-test" not in span_tag.attrs.keys():
             continue
-        article = requests.get(f"{user_url + tag.a['href']}")
-        article_soup = BeautifulSoup(article.content, "lxml")
+        article_link = user_url + tag.a["href"]
+        article_soup = get_web_response(article_link)
         article_content = article_soup.find_all("p")
 
         for article_tag in article_content:
             print(f"\n{article_tag}")
             print("-------------------------------------------------------------------------------------------------")
             print("-------------------------------------------------------------------------------------------------\n")
-else:
-    print(f"The URL returned {response.status_code}!")
+
+
+def get_web_response(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "lxml")
+        return soup
+    return response.status_code
+
+
+if __name__ == "__main__":
+    main()
