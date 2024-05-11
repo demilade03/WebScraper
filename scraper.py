bc4e14ed9fddae0d5194d6716d72830246b3b9df
demilade03 <odemilade118@gmail.com>
2024-05-11 13:15:32 +0100

    Accessing individual tag attributes, child tags and string

--- scraper.py
+++ scraper.py
@@ -5,18 +5,28 @@ from bs4 import BeautifulSoup
 def main():
     user_url = input("Enter a URL: ")
     soup = get_web_response(user_url)
-    for tag in get_tags(soup):
-        print(tag)
+    tag_list = get_tags(soup)
+    for num, tag in enumerate(tag_list):
+        print(f"{num + 1}, {tag}")
+    user_tag = int(input("\nWhat tag will you like to inspect (Enter the corresponding number): "))
+    print(get_tag_elements(soup, tag_list, user_tag))
 
 
 def get_tags(obj):
-    tags = {}
+    tags = []
     for tag in obj.find_all():
-        if tag.name not in tags:
-            tags[tag.name] = 1
-        else:
-            tags[tag.name] += 1
+        if tag.name in tags:
+            continue
+        tags.append(tag.name)
     return tags
+
+
+def get_tag_elements(soup, tag_list, tag_number):
+    active_tag = soup.find(tag_list[tag_number - 1])
+    active_tag_attr = active_tag.attrs.keys()
+    active_tag_list = get_tags(active_tag)
+    active_tag_string = active_tag.string if active_tag.string else "N/A"
+    return active_tag_attr, active_tag_list, active_tag_string
 
 
 def get_web_response(url):
