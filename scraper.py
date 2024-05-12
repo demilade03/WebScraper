420efefbfab229516721bb07fa81f66e6ba251f9
demilade03 <odemilade118@gmail.com>
2024-05-12 14:54:21 +0100

    Simplified getting tag attributes, child tags and string

--- scraper.py
+++ scraper.py
@@ -5,14 +5,21 @@ from bs4 import BeautifulSoup
 def main():
     user_url = input("Enter a URL: ")
     soup = get_web_response(user_url)
-    tag_list = get_tags(soup)
-    for num, tag in enumerate(tag_list):
-        print(f"{num + 1}, {tag}")
-    user_tag = int(input("\nWhat tag will you like to inspect (Enter the corresponding number): "))
-    print(get_tag_elements(soup, tag_list, user_tag))
+    if isinstance(soup, int):
+        print(soup)
+    else:
+        tag_list = get_child_tags(soup)
+        for num, tag in enumerate(tag_list):
+            print(f"{(num + 1):3d}. {tag}")
+        user_tag = int(input("\nWhat tag will you like to inspect (Enter the corresponding number): "))
+        tag_elements = get_tag_elements(soup, tag_list, user_tag)
+        print(f"Attributes in tag '{tag_elements.name}': {get_tag_attributes(tag_elements)}")
+        print(f"Child tags in tag '{tag_elements.name}': {get_child_tags(tag_elements)}")
+        print(f"String in tag '{tag_elements.name}': {get_tag_string(tag_elements)}" if get_tag_string(tag_elements)
+              else "No Strings in this tag")
 
 
-def get_tags(obj):
+def get_child_tags(obj):
     tags = []
     for tag in obj.find_all():
         if tag.name in tags:
@@ -21,12 +28,19 @@ def get_tags(obj):
     return tags
 
 
-def get_tag_elements(soup, tag_list, tag_number):
-    active_tag = soup.find(tag_list[tag_number - 1])
-    active_tag_attr = active_tag.attrs.keys()
-    active_tag_list = get_tags(active_tag)
-    active_tag_string = active_tag.string if active_tag.string else "N/A"
-    return active_tag_attr, active_tag_list, active_tag_string
+def get_tag_elements(tag, tag_list, tag_number):
+    active_tag = tag.find(tag_list[tag_number - 1])
+    return active_tag
+
+
+def get_tag_attributes(tag):
+    tag_attr = [key for key in tag.attrs.keys()]
+    return tag_attr
+
+
+def get_tag_string(tag):
+    tag_string = tag.string if tag.string else ""
+    return tag_string
 
 
 def get_web_response(url):
