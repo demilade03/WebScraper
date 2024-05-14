b80927d092d1ce52487398c24994979d6750aff7
demilade03 <odemilade118@gmail.com>
2024-05-14 12:27:25 +0100

    Getting all tags that match user's selected tag

--- scraper.py
+++ scraper.py
@@ -13,37 +13,44 @@ def main():
             print(f"{(num + 1):3d}. {tag}")
         user_tag = int(input("\nWhat tag will you like to inspect (Enter the corresponding number): "))
         tag_elements = get_tag_elements(soup, tag_list, user_tag)
-        print(f"Attributes in tag '{tag_elements.name}': {get_tag_attributes(tag_elements)}")
-        print(f"Child tags in tag '{tag_elements.name}': {get_child_tags(tag_elements)}")
-        print(f"String in tag '{tag_elements.name}': {get_tag_string(tag_elements)}" if get_tag_string(tag_elements)
-              else "No Strings in this tag")
+        for tag in tag_elements:
+            print(f"Attributes in tag '{tag.name}': {get_tag_attributes(tag)}")
+            print(f"Child tags in tag '{tag.name}': {get_child_tags(tag)}")
+            print(f"String in tag '{tag.name}': {get_tag_string(tag)}" if get_tag_string(tag)
+                  else "No strings in this tag")
+            print("\n-----------------------------------------------------------------------------------------------\n")
 
 
-def get_child_tags(obj):
+def get_child_tags(tag):
+    """Get all the child tags of a particular tag."""
     tags = []
-    for tag in obj.find_all():
-        if tag.name in tags:
+    for child_tag in tag.find_all():
+        if child_tag.name in tags:
             continue
-        tags.append(tag.name)
+        tags.append(child_tag.name)
     return tags
 
 
 def get_tag_elements(tag, tag_list, tag_number):
-    active_tag = tag.find(tag_list[tag_number - 1])
-    return active_tag
+    """Get all tags that match the user's tag."""
+    for active_tag in tag.find_all(tag_list[tag_number - 1]):
+        yield active_tag
 
 
 def get_tag_attributes(tag):
+    """Get the attributes within a tag."""
     tag_attr = [key for key in tag.attrs.keys()]
     return tag_attr
 
 
 def get_tag_string(tag):
-    tag_string = tag.string if tag.string else ""
+    """Get the string within a tag."""
+    tag_string = tag.string
     return tag_string
 
 
 def get_web_response(url):
+    """Get the webpage document and parse it."""
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "lxml")
